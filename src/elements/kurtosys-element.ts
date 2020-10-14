0fb48e650a9d9d284c6ed71f8d076d256f05de55
Jan Kuźniak <jan.kuzniak@gmail.com>
2020-10-14 19:02:11 +0200

    Added configuration properties to the Kurtosys element

--- src/elements/kurtosys-element.ts
+++ src/elements/kurtosys-element.ts
@@ -2,6 +2,9 @@ import { Context } from '../context'
 import { RESTElementHandler } from './rest-element'
 
 export class KurtosysElementHandler extends RESTElementHandler {
+	kurtosysToken: string = 'fc92cd68-b71c-4994-8e23-da1971402ae8'
+	isin: string = 'GB00BD6FFQ03'
+
 	constructor(context: Context) {
 		super(context)
 	}
@@ -12,11 +15,11 @@ export class KurtosysElementHandler extends RESTElementHandler {
 		return fetch(url, {
 			method: 'POST',
 			headers: {
-				'X-KSYS-TOKEN': 'fc92cd68-b71c-4994-8e23-da1971402ae8',
+				'X-KSYS-TOKEN': this.kurtosysToken,
 				'Content-Type': 'application/json',
 				Accept: 'application/json',
 			},
-			body: '{"type":"CLSS","search":[{"property":"isin","values":["GB00BD6FFQ03"],"matchtype":"MATCH"}],"include":{"timeseries":{},"allocations":{},"statistics":{},"disclaimers":{},"commentaries":{},"documents":{"joinSpec":[{"joinTo":"isin"}]}},"culture":"en-GB","applyFormats":false,"start":0,"limit":10,"fundList":"view_uk_pri_OEIC","sort":{"key":"infinite_sort","direction":"ASC"}}',
+			body: '{"type":"CLSS","search":[{"property":"isin","values":["' + this.isin + '"],"matchtype":"MATCH"}],"include":{"timeseries":{},"disclaimers":{}},"culture":"en-GB","applyFormats":false,"start":0,"limit":1,"fundList":"view_uk_pri_OEIC","sort":{"key":"infinite_sort","direction":"ASC"}}',
 			cf: { cacheTtl: this.cacheTTL, cacheEverything: true },
 		});
 	}

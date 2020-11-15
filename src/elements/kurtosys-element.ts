14fbc04314c5282f9366d2c33714c37da911d703
David Friar <david.friar@cognifide.com>
2020-11-15 16:26:23 +0000

    Use 'edgeside' npm package

--- src/elements/kurtosys-element.ts
+++ src/elements/kurtosys-element.ts
@@ -1,26 +1,29 @@
-import { Context } from '../context'
-import { RESTElementHandler } from './rest-element'
+import { Context } from 'edgeside'
+import { RESTElementHandler } from 'edgeside'
 
 export class KurtosysElementHandler extends RESTElementHandler {
-	kurtosysToken: string = 'fc92cd68-b71c-4994-8e23-da1971402ae8'
-	isin: string = 'GB00BD6FFQ03'
+  kurtosysToken: string = 'fc92cd68-b71c-4994-8e23-da1971402ae8'
+  isin: string = 'GB00BD6FFQ03'
 
-	constructor(context: Context) {
-		super(context)
-	}
+  constructor(context: Context) {
+    super(context)
+  }
 
-	fetchData(): Promise<Response> {
-		const url = this.getDataURL()
+  fetchData(): Promise<Response> {
+    const url = this.getDataURL()
 
-		return fetch(url, {
-			method: 'POST',
-			headers: {
-				'X-KSYS-TOKEN': this.kurtosysToken,
-				'Content-Type': 'application/json',
-				Accept: 'application/json',
-			},
-			body: '{"type":"CLSS","search":[{"property":"isin","values":["' + this.isin + '"],"matchtype":"MATCH"}],"include":{"timeseries":{},"disclaimers":{}},"culture":"en-GB","applyFormats":false,"start":0,"limit":1,"fundList":"view_uk_pri_OEIC","sort":{"key":"infinite_sort","direction":"ASC"}}',
-			cf: { cacheTtl: this.cacheTTL, cacheEverything: true },
-		});
-	}
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'X-KSYS-TOKEN': this.kurtosysToken,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body:
+        '{"type":"CLSS","search":[{"property":"isin","values":["' +
+        this.isin +
+        '"],"matchtype":"MATCH"}],"include":{"timeseries":{},"disclaimers":{}},"culture":"en-GB","applyFormats":false,"start":0,"limit":1,"fundList":"view_uk_pri_OEIC","sort":{"key":"infinite_sort","direction":"ASC"}}',
+      cf: { cacheTtl: this.cacheTTL, cacheEverything: true },
+    })
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,24 @@
-import { handleRequest } from './request-handler'
-import { Config } from './types'
-import { DebugElementHandler } from './elements'
-import { KurtosysElementHandler } from './elements'
-
-const config: Config = {
-  elements: [
-    // custom element handlers here e.g.
-    ['debug', DebugElementHandler],
-    ['securerest', KurtosysElementHandler],
-  ],
-  urlRewriteRules: [
-    // custom rewrite rules here
-    [
-      'welcome-to-the-edge/.*/country-matters.html',
-      'welcome-to-the-edge/country-matters.html',
-    ],
-    ['demo1/.*/country.html', 'demo1/country.html'],
-    ['demo1/.*/continent.html', 'demo1/continent.html'],
-    ['demo1/.*/continent.html', 'demo1/continent.html'],
-    ['demo1/.*/time.html', 'demo1/time.html'],
-    [
-      '/etc/cloudsettings.kernel.js/libs/settings/cloudsettings/legacy/contexthub',
-      'hacktostopthisloading',
-    ],
-    [
-      'examples/elements/graphql/.*/example3',
-      'examples/elements/graphql/example3',
-    ],
-    ['examples/elements/rest/.*/example3', 'examples/elements/rest/example3'],
-  ],
-  converters: {
-    //custom converters here
-  },
-}
+import { handleRequest } from 'edgeside'
+import { KurtosysElementHandler } from './elements/kurtosys-element'
 
 addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event.request, config))
+  event.respondWith(
+    handleRequest(event.request, {
+      elements: [['securerest', KurtosysElementHandler]],
+      urlRewriteRules: [
+        [
+          'welcome-to-the-edge/.*/country-matters.html',
+          'welcome-to-the-edge/country-matters.html',
+        ],
+        ['demo1/.*/country.html', 'demo1/country.html'],
+        ['demo1/.*/continent.html', 'demo1/continent.html'],
+        ['demo1/.*/continent.html', 'demo1/continent.html'],
+        ['demo1/.*/time.html', 'demo1/time.html'],
+        [
+          '/etc/cloudsettings.kernel.js/libs/settings/cloudsettings/legacy/contexthub',
+          'hacktostopthisloading',
+        ],
+      ],
+    }),
+  )
 })
